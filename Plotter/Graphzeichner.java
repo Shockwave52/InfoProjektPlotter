9c69dc5df3d882eb8a5ce7db3909ebd0bfe4088e
Shockwave52 <Magic-cloud-52@hotmail.de>
2015-01-20 14:33:29 +0100

    graphzeichner fixes
    -x und y Achse werden jetzt richtig gezeichnet
    -punktverbindungslogik auf arraylist umgeschreiben
    HOWEVER, es wird nachwievor nur einmal gemahlt beim starten gemahlt

--- Plotter/Graphzeichner.java
+++ Plotter/Graphzeichner.java
@@ -10,9 +10,10 @@ public class Graphzeichner extends JPanel {
 	//standard Koordinatensystem
 	private int yMax = 5;
 	private int yMin=-5;
-	private int xMin=-5;
 	private int xMax = 5;
+	private int xMin=-5;
 	private int skal=1;
+	private int zoom=10;
 	private int yAchseZero,xAchseZero,yRange,xRange;
 	private String eingabe;
 		
@@ -24,14 +25,15 @@ public class Graphzeichner extends JPanel {
 		yRange=(yMax-yMin);
 		xRange=(xMax-xMin);
 		
+		//fenster von oben links z.B 0,0 nach unten rechts z.B.10,10
 		//g.drawLine(x1, y1, x2, y2);
 		g.drawLine(0,0,0,yRange); //Rahmen ver. Links
 		g.drawLine(0,yRange,xRange,yRange); //Rahmen hor. Unten
 		g.drawLine(xRange,yRange,xRange,0); //Rahmen ver. rechts
 		g.drawLine(xRange,0,0,0); //Rahmen hor. oben
 				
-		yAchseZero=yRange/2;
-		xAchseZero=xRange/2;
+		yAchseZero=yMax;
+		xAchseZero=xMin*-1;
 	
 		g.drawLine(0,yAchseZero,xRange,yAchseZero); //X-Achse
 		g.drawLine(xAchseZero,0,xAchseZero,yRange); //Y-Achse
@@ -49,16 +51,16 @@ public class Graphzeichner extends JPanel {
 		}
 		
 		/*
-		array[]=calculate_oder_plot_was_auch_immer(); 
-		for (int i=0;i<arraycount;)
+		ArrayList<Int> ergebnisse = calculate_oder_plot_was_auch_immer();
+		for (int i=0;i<ergebnisse.size;)
 		{
-			if (((array[i]+skalierung) < yRange && (array[i]+skalierung) > 0)&&((array[i+1]+skalierung) < yRange && (array[i+1]+skalierung) > 0)) 
+			if (((ergebnisse.get(i)+skalierung) < yRange && (ergebnisse.get(i)+skalierung) > 0)&&((ergebnisse.get(i+1)+skalierung) < yRange && (ergebnisse.get(i+1)+skalierung) > 0)) 
 			then
 			{
 			int x1= skalierung*i;
-			int y1= array[i]+xAchseZero;
+			int y1= ergebnisse.get(i);
 			int x2; skalierung*(i+1);
-			int y2= array[i+1]+xAchseZero;
+			int y2= ergebnisse.get(i+1)+xAchseZero;
 			g.drawLine(x1,y1,x2,y2);
 			i++;
 			}
@@ -80,11 +82,11 @@ public class Graphzeichner extends JPanel {
 		eingabe=eingabeHilf;
 		
 		//Zoom sozusagen weil sonst alles zu klein ist
-		yMax=yMax*10;
-		yMin=yMin*10;
-		xMin=xMin*10;
-		xMax=xMax*10;
-		skal=skal*10;
+		yMax=yMax*zoom;
+		yMin=yMin*zoom;
+		xMin=xMin*zoom;
+		xMax=xMax*zoom;
+		skal=skal*zoom;
 	}
 	
 	public void plot(String eingabe,int xMin,int xMax, JLabel debugFeld)
